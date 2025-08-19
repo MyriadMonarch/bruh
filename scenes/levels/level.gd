e3657845376fc3322cc47512676f63ab22bd9219
MyriadMonarch <dgantavya@gmail.com>
2025-08-19 08:40:02 +0545

    script

---
+++ scenes/levels/level.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _ready():
+	print('level node is ready')
+	
+
+
+func _process(delta):
+	pass
--- scenes/levels/logo.gd
+++ scenes/levels/logo.gd
@@ -1,13 +1,19 @@
 extends Sprite2D
 var pos: Vector2 = Vector2.ZERO  #(0,0)
+var sca: float = 1
 const speed: int = 10
 
 func _ready():
 	pos = Vector2(65,65)
-	position = pos
+	
+	var test_rotation = 45
+	rotation_degrees = test_rotation
 
 
 
 func _process(delta):
 	pos.x += 10
 	position = pos
+	sca += .01
+	scale = Vector2(sca,sca)
+	
