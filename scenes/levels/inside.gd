6322263e0a10ca870485d7bfa4e01859cda4d25d
MyriadMonarch <dgantavya@gmail.com>
2025-08-31 15:26:14 +0545

    ui

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,11 +1,9 @@
 extends LevelParent
 
+@export var outside_level_scene: PackedScene = preload("res://scenes/levels/outside.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_exit_gate_area_body_entered(_body):
+	var tween = create_tween() 
+	tween.tween_property($Player,"speed",0,.5)
+	#get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	get_tree().change_scene_to_packed(outside_level_scene)
