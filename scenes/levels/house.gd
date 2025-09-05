82bc17510fe6a8e78fe8b16405f5842ef51b6eaa
MyriadMonarch <dgantavya@gmail.com>
2025-09-05 14:26:59 +0545

    dhgf

--- scenes/levels/house.gd
+++ scenes/levels/house.gd
@@ -2,10 +2,10 @@ extends Area2D
 signal player_exited
 signal player_entered
 
-func _on_body_entered(_body):
-	player_entered.emit()
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	player_entered.emit()  # Replace with function body.
 
 
-
-func _on_body_exited(_body):
-	player_exited.emit() # Replace with function body.
+func _on_body_exited(body: CharacterBody2D) -> void:
+	player_exited.emit()  # Replace with function body.
